d75507e4f82a0e7aaf8e1e6d65aa4bc501796cd3
Kelly Willard <kellywillard@Kellys-MacBook-Pro.local>
2019-10-23 20:07:14 -0700

    Finished pseudoclassical pattern

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,21 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
 };
 
+Stack.prototype.push = function(value) {
+  var newKey = (Object.keys(this.storage).length) + 1;
+  this.storage[newKey] = value;
+  return this;
+};
 
+Stack.prototype.pop = function() {
+  var lastKey = Object.keys(this.storage).length;
+  var value = this.storage[lastKey];
+  delete this.storage[lastKey];
+  return value;
+};
+
+Stack.prototype.size = function() {
+  var keys = Object.keys(this.storage);
+  return keys.length;
+};
