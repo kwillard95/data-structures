111bd2028571564bb384a65c5fd681b35d2e6c1c
Kelly Willard <kellywillard@Kellys-MacBook-Pro.local>
2019-10-23 20:19:53 -0700

    Completed part one sprint

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,28 @@
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+  }
+  enqueue(value) {
+    var newKey = (Object.keys(this.storage).length) + 1;
+    this.storage[newKey] = value;
+    return this;
   }
 
+  dequeue() {
+    var firstKey = Object.keys(this.storage)[0];
+    var value = this.storage[firstKey];
+    delete this.storage[firstKey];
+    for (var key in this.storage) {
+      var num = parseInt(key) - 1;
+      var newKey = num.toString();
+      this.storage[newKey] = this.storage[key];
+      delete this.storage[key];
+    }
+    return value;
+  }
+
+  size() {
+    var length = Object.keys(this.storage).length;
+    return length;
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,24 @@
 class Stack {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+  }
+
+  push(value) {
+    var newKey = (Object.keys(this.storage).length) + 1;
+    this.storage[newKey] = value;
+    return this;
+  }
+
+  pop() {
+    var lastKey = Object.keys(this.storage).length;
+    var value = this.storage[lastKey];
+    delete this.storage[lastKey];
+    return value;
+  }
+
+  size() {
+    var keys = Object.keys(this.storage);
+    return keys.length;
   }
 
 }
