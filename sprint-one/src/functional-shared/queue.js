3961ce0dd5dbf07eb090212962639f0557db7046
Jessica Bainbridge-Smith <publicjbs@gmail.com>
2019-10-23 18:40:05 -0700

    Completed stack and queue instantiators for the functional-shared pattern

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,34 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+  extend(someInstance, queueMethods);
+  return someInstance;
+};
+
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this.storage[Object.keys(this.storage).length] = value;
+};
 
+queueMethods.dequeue = function() {
+  var item = this.storage[0];
+  delete this.storage[0];
+  for (var key in this.storage) {
+    this.storage[key - 1] = this.storage[key];
+  }
+  delete this.storage[Object.keys(this.storage).length - 1];
+  return item;
+};
+
+queueMethods.size = function() {
+  return Object.keys(this.storage).length;
+};
