7a61a5b9c8f82c4dc802d266bbc66ee4f0514dc7
kwillard95 <kwillard95@gmail.com>
2019-10-26 10:47:10 -0700

    Completed use case for Binary Tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -53,3 +53,33 @@ BinarySearchTree.prototype.depthFirstLog = function(callback) {
 // Insert and contains are O(log n)
 // depthFirstLog is linear O(n)
 
+var findNearest = function(array, target) {
+  //create our tree
+  var tree = BinarySearchTree(array[0]);
+  array.forEach(function(item) {
+    tree.insert(item);
+  });
+  //search our tree
+  //var closest value
+  var closestValue = Infinity;
+  var prevDiff = Infinity;
+
+  var checkNode = function(node) {
+    var currDiff = Math.abs(target - node.value);
+    if (currDiff < prevDiff) {
+      closestValue = node.value;
+    }
+    if (target > node.value && node.right) {
+      checkNode(node.right);
+    } else if (target < node.value && node.left) {
+      checkNode(node.left);
+    }
+  };
+  checkNode(tree);
+  return closestValue;
+};
+
+var testArray = [8, 10, 3, 1, 6, 14, 7, 4, 13];
+console.log(findNearest(testArray, 12));
+
+//We would have to traverse through the entire array of potentially a million numbers in order to know the closest number. With the binary tree we only have to traverse through the levels of one path. If we were to traverse through an array the time complexity would be linear, whereas the time complexity of traversing through a binary tree is O(log n)
